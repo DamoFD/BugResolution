810dc3f3d3bea9c0189547d3409af2ca6fb41b60
Surulere Chris. I. Abiye <suruabiye@gmail.com>
2023-08-27 20:07:10 +0100

    CRUD for Questions

--- database/migrations/2023_08_18_053114_create_questions_table.php
+++ database/migrations/2023_08_18_053114_create_questions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->string('title');
             $table->text('body');
             $table->timestamps();
